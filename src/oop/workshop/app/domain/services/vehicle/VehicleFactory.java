0bddbedf8cc99f5b29f8c6d4e81fc37bda41d37a
Ismael Trocha <ismael.trocha@hotmail.com>
2024-03-07 22:01:04 -0500

    Documentation Added: I write a little explanation for the Factory Method Principle.

--- src/oop/workshop/app/domain/services/vehicle/VehicleFactory.java
+++ src/oop/workshop/app/domain/services/vehicle/VehicleFactory.java
@@ -2,6 +2,11 @@ package oop.workshop.app.domain.services.vehicle;
 
 import oop.workshop.app.domain.model.Vehicle;
 
+/*
+* Este es una interfaz la cual esta cumpliendo con un patron de diseño llamado
+* Factory Method, el cual es un patrón creacional que nos permite manejar
+* Una correcta creación de objetos especificos.
+* */
 public interface VehicleFactory <T extends Vehicle>{
 
   T createVehicle();
